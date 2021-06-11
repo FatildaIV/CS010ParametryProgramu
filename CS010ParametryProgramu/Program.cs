a70529c77683588fad9c46b2c81762efcc941a34
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 11:16:28 +0200

    Add files via upload

---
+++ CS010ParametryProgramu/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS010ParametryProgramu
+{
+    class Program
+    {
+        static void JmenoVypis()
+        {
+            Console.Write("Karel");
+        }
+        static void Mezera()
+        {
+            Console.Write(" ");
+        }
+        static void PrijmeniVypis()
+        {
+            Console.Write("Hofman");
+        }
+        static void Vypis(string vypis)
+        {
+            Console.Write(vypis);
+        }
+        static void VypisJmenoPrijmeni()
+        {
+            JmenoVypis();
+            Console.Write(" ");
+            PrijmeniVypis();
+        }
+        static void VypisJmenoPrijmeni(string jmeno, string prijmeni)
+        {
+            Console.Write(jmeno);
+            Console.Write(" ");
+            Console.Write(prijmeni);
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                VypisJmenoPrijmeni();
+            }
+            else if (args.Length == 2)
+            {
+                VypisJmenoPrijmeni(args[0], args[1]);
+            }
+            JmenoVypis();
+            Mezera();
+            PrijmeniVypis();
+            Mezera();
+            Vypis("2.D");
+            Console.ReadKey();
+        }
+    }
+}
